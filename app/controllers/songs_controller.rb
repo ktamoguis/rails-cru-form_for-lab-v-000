a748b092c142c86da040ae5564f5783267451f1c
Karl Amoguis <ktamoguis@gmail.com>
2018-07-09 07:11:04 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    #binding.pry
+    binding.pry
     @song = Song.create(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
